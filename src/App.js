4dd8fcb134b9ee6e9a3dc02141dd35d9b71d250a
James Morris <blondfro@gmail.com>
2020-03-14 13:58:29 -0500

    created a simple button toggle to make the logo either spin or not.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.png";
 import "./App.css";
 
 function App() {
+  const [toggleLogo, setToggleLogo] = useState(true);
+
+  const onToggleLogo = () => {
+    setToggleLogo(!toggleLogo);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className={toggleLogo ? "App-logo" : "static-logo"}
+          alt="logo"
+          onClick={onToggleLogo}
+        />
         <h1>Welcome to React</h1>
+        <button onClick={onToggleLogo}>Click Me</button>
       </header>
     </div>
   );
