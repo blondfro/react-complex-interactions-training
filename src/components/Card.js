3829b49fcf453b1e0c188aad6673f9761e98a75b
James Morris <blondfro@gmail.com>
2020-03-14 20:39:02 -0500

    implemented react-lazyload.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,32 +1,36 @@
 import React from "react";
+import LazyLoad from "react-lazyload";
 import "./Card.css";
 
 function Card({ duration, card, showFront, showBack, ...props }) {
   return (
-    <div className={card.animation}>
-      <div className="front" onClick={() => showBack(card)}>
-        <img
-          src={process.env.PUBLIC_URL + "/juice.jpg"}
-          alt="Vitamin Juice"
-          className="card-image"
-        />
-        <div className="container">
+    <LazyLoad height={650} offset={-100}>
+      <div className={card.animation}>
+        <div className="front" onClick={() => showBack(card)}>
+          <img
+            src={process.env.PUBLIC_URL + "/juice.jpg"}
+            alt="Vitamin Juice"
+            className="card-image"
+          />
+          <div className="container">
+            <h3>
+              Vitamin Juice <span className="price">$24.99</span>
+            </h3>
+            <p>
+              Need a jump on your vitamins while drinking? Tired of popping
+              pills? Drink our Vitamin enhanced juice! Available in several
+              flavors!
+            </p>
+          </div>
+        </div>
+        <div className="container-back back" onClick={() => showFront(card)}>
           <h3>
             Vitamin Juice <span className="price">$24.99</span>
           </h3>
-          <p>
-            Need a jump on your vitamins while drinking? Tired of popping pills?
-            Drink our Vitamin enhanced juice! Available in several flavors!
-          </p>
+          <p>{card.description}</p>
         </div>
       </div>
-      <div className="container-back back" onClick={() => showFront(card)}>
-        <h3>
-          Vitamin Juice <span className="price">$24.99</span>
-        </h3>
-        <p>{card.description}</p>
-      </div>
-    </div>
+    </LazyLoad>
   );
 }
 
